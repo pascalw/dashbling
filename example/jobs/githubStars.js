6d6e839f9c2587ecd4423b07848b6d7d01e46a81
Pascal Widdershoven <pascal@kabisa.nl>
2017-12-10 18:49:14 +0100

    Prevent unhandled promise rejection warnings

--- example/jobs/githubStars.js
+++ example/jobs/githubStars.js
@@ -2,8 +2,12 @@ const fetch = require("node-fetch");
 
 module.exports = (repo, eventId) =>
   async function gitHubStars(sendEvent) {
-    const response = await fetch(`https://api.github.com/repos/${repo}`);
-    const json = await response.json();
+    try {
+      const response = await fetch(`https://api.github.com/repos/${repo}`);
+      const json = await response.json();
 
-    sendEvent(eventId, json);
+      sendEvent(eventId, json);
+    } catch (e) {
+      console.warn("Failed to fetch GitHub stars", e);
+    }
   };
