2abfb9bfc925a558aece3457f322f1647fd4524e
Pascal Widdershoven <pascal@kabisa.nl>
2017-12-17 19:51:24 +0100

    Add dashboard generator

---
+++ packages/create-dashbling-app/create-dashboard.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const yargs = require("yargs");
+const { createEnv } = require("yeoman-environment");
+const { resolve, basename } = require("path");
+
+const env = createEnv();
+const done = exitCode => process.exit(exitCode || 0);
+const dashboardGenerator = resolve(__dirname, "./generators/dashboard");
+env.register(require.resolve(dashboardGenerator), "create-dashboard");
+
+const cli = yargs
+  .command("<dashboard-directory>")
+  .demandCommand(1)
+  .help()
+  .wrap(null).argv;
+
+const directory = resolve(cli._[0]);
+const name = basename(directory);
+
+env.run(
+  "create-dashboard",
+  {
+    directory,
+    name
+  },
+  done
+);
