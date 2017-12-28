e9b4667e0965b0383f4db7ced0b48be5eebc994b
Pascal Widdershoven <hello@pascalw.me>
2017-12-28 19:27:06 +0100

    Run e2e tests with real packages
    This should help catch issues caused by package configuration issues.

--- script/e2e-tests.sh
+++ script/e2e-tests.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 set -e
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-pushd ./packages/build-support/
-  yarn build
-popd
+packageVersion() {
+  node -e "console.log(require('./packages/$1/package.json').version)"
+}
 
-pushd ./packages/core/
-  yarn build
-popd
+packagePath() {
+  echo "file:$SCRIPTPATH/../packages/$1/dashbling-$1-$(packageVersion "$1").tgz"
+}
+
+yarn lerna exec npm pack
+
+export DASHBLING_CORE_PACKAGE=$(packagePath "core")
+export DASHBLING_BUILD_SUPPORT_PACKAGE=$(packagePath "build-support")
+export DASHBLING_CLIENT_PACKAGE=$(packagePath "client")
 
 pushd ./packages/create-dashbling-app/
   yarn install
